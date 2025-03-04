61641e014c13f9ec79a7aa1c8bb798ab9b4c57d5
David39jq <brayansolis@unicomfacauca.edu.co>
2025-03-04 16:06:00 -0500

    Se agrega nav lateral y en la pagina index, y otros

--- src/home/js/nav.js
+++ src/home/js/nav.js
@@ -1,15 +1,15 @@
-// Toggle the visibility of a dropdown menu
+// Alternar la visibilidad de un menú desplegable
 const toggleDropdown = (dropdown, menu, isOpen) => {
     dropdown.classList.toggle("open", isOpen);
     menu.style.height = isOpen ? `${menu.scrollHeight}px` : 0;
   };
-  // Close all open dropdowns
+  //Cerrar todos los menús desplegables abiertos
   const closeAllDropdowns = () => {
     document.querySelectorAll(".dropdown-container.open").forEach((openDropdown) => {
       toggleDropdown(openDropdown, openDropdown.querySelector(".dropdown-menu"), false);
     });
   };
-  // Attach click event to all dropdown toggles
+  // Adjuntar evento de clic a todos los menús desplegables
   document.querySelectorAll(".dropdown-toggle").forEach((dropdownToggle) => {
     dropdownToggle.addEventListener("click", (e) => {
       e.preventDefault();
@@ -20,12 +20,12 @@ const toggleDropdown = (dropdown, menu, isOpen) => {
       toggleDropdown(dropdown, menu, !isOpen); // Toggle current dropdown visibility
     });
   });
-  // Attach click event to sidebar toggle buttons
+  // Adjuntar evento de clic a los botones de alternancia de la barra lateral
   document.querySelectorAll(".sidebar-toggler, .sidebar-menu-button").forEach((button) => {
     button.addEventListener("click", () => {
       closeAllDropdowns(); // Close all open dropdowns
       document.querySelector(".sidebar").classList.toggle("collapsed"); // Toggle collapsed class on sidebar
     });
   });
-  // Collapse sidebar by default on small screens
+  // Contraer la barra lateral de forma predeterminada en pantallas pequeñas
   if (window.innerWidth <= 1024) document.querySelector(".sidebar").classList.add("collapsed");

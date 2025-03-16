f36753e6afa2774dfa726fae776e953e0ad2989f
victorgilon <victorgilon@unicomfacauca.edu.co>
2025-03-16 18:47:38 -0500

    encriptacion de contraseña para mas seguridad de los usuarios e instalación de bcrypt

--- rutas/regUsuario.js
+++ rutas/regUsuario.js
@@ -2,24 +2,33 @@ const express = require("express");
 const router = express.Router();
 const conexion = require("../config/conexion");
 const link = require("../config/link");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
-router.post("/regUsuario", function(req, res){
+router.post("/regUsuario", async function(req, res){
     let nom = req.body.campoNom;
     let ema = req.body.campoEma;
     let pass = req.body.campoPass;
 
-    const insertar = "INSERT INTO usuario ( NOMBRE, EMAIL, PASSWORD) VALUES ('"+nom+"', '"+ema+"', '"+pass+"');"
+    try {
+        const hashedpas = await bcrypt.hash(pass, saltRounds);
+        const insertar = "INSERT INTO usuario ( NOMBRE, EMAIL, PASSWORD) VALUES (?, ?, ?);"
 
-    conexion.query(insertar, function(error, row){
-        if(error){
-            console.log("Error al interntar registrar usuario");
-            throw error;
-        }else{
-            console.log("Registro exitoso");
-            mensaje = "Registro exitoso, ya puedes iniciar sesion";
-            res.render("index",{mensaje, link});
-        }
-    });
-});
+        conexion.query(insertar, [nom, ema, hashedpas], function(err){
+            if(err){
+                console.log("Error al interntar registrar usuario");
+                return res.status(500).send("Error al registrar usuario");
+            }else{
+                console.log("Registro exitoso");
+                let mensaje = "Registro exitoso, ya puedes iniciar sesion";
+                res.render("index", {mensaje, link});
+            }    
+        })
+    } catch (error) {
+        console.error("Error al registrar", error);
+        res.status(500).send("Error en el servidor");
+
+    }
+})
 
 module.exports = router;

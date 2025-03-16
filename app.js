7c09aaaaae4e7ac2ba3eaee9211129e1e83a9e6f
victorgilon <victorgilon@unicomfacauca.edu.co>
2025-03-16 15:01:59 -0500

    conexion bd restaurante
    conexion, instalacion, nodejs, express, ejs

---
+++ app.js
@@ -0,0 +1,24 @@
+// importar librerias 
+const express = require("express");
+const app = express();
+
+
+//configuraciones
+app.set("view engine", "ejs");//paginas dinamicas
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+//rutas::::::::::::::::::::::::::::::::::::::::
+app.use(express.static("public"));
+
+
+//configurando el puesto del servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function(){
+    if(PORT == 3000){
+        console.log("http://localhost:3000");
+    }else{
+        console.log(PORT);
+    }
+})
+
---
+++ config/conexion.js
@@ -0,0 +1,18 @@
+let conectar = require("mysql");
+
+let conexion = conectar.createConnection({
+    host: "localhost",
+    database: "restaurante",
+    user: "root",
+    password: "reserva"
+});
+
+conexion.connect(function(error){
+    if(error){
+        throw error;
+    }else{
+        console.log("conexion exitosa");
+    }
+});
+
+module.exports = conexion;

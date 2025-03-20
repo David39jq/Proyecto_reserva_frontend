1d256ba09a8c1e79455616d7a8448f99b12b1427
David39jq <brayansolis@unicomfacauca.edu.co>
2025-03-20 11:27:54 -0500

    se arreglo colores en login y registro

--- public/js/login.js
+++ public/js/login.js
@@ -1,11 +1,19 @@
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
+/*=============== MOSTRAR OCULTAR CONTRASEÑA INICIAR SESIÓN ================*/
+const passwordAccess = (loginPass, loginEye) =>{
+   const input = document.getElementById(loginPass),
+         iconEye = document.getElementById(loginEye)
 
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
-});
+   iconEye.addEventListener('click', () =>{
+      // Cambiar contraseña a texto
+      input.type === 'password' ? input.type = 'text'
+						              : input.type = 'password'
 
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
-});
+      // cambio de icono
+      iconEye.classList.toggle('ri-eye-fill')
+      iconEye.classList.toggle('ri-eye-off-fill')
+   })
+}
+passwordAccess('password','loginPassword')
+
+
+
